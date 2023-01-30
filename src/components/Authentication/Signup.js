2be693e1ea0d0775c79c7dc34e92fc0043c46c91
NyanPham <phamthanhnhanussh@gmail.com>
2023-01-30 20:37:05 +0700

    updated the alert for signup

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,14 +1,20 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import axios from 'axios'
 import loginBackground from '../../imgs/ocean.jpg'
 import { useAuthContext } from '../../context/authContext'
 import { useNavigate } from 'react-router-dom'
 import { ROOT_URL } from '../../config'
+import LoadingWithAlert from '../LoadingWithAlert'
 
 // Need to move the image background render to the className later
 function Signup() {
     const { authLogin } = useAuthContext()
     const navigate = useNavigate()
+
+    const [showAlert, setShowAlert] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [message, setMessage] = useState('')
+    const [error, setError] = useState('')
 
     const nameRef = useRef()
     const emailRef = useRef()
@@ -27,6 +33,10 @@ function Signup() {
 
         if (password !== passwordConfirm) return alert('Passwords do not match!')
 
+        setIsLoading(true)
+        setMessage('')
+        setError('')
+
         try {
             const res = await axios({
                 method: 'POST',
@@ -40,12 +50,16 @@ function Signup() {
                 withCredentials: true,
             })
             if (res.data.status === 'success') {
-                alert('You have signed up successfully!')
+                setMessage('You have signed up successfully!')
                 authLogin()
                 navigate('/')
             }
         } catch (err) {
-            alert(err.response.data.message)
+            console.log(err)
+            setError(err.response.data.message)
+        } finally {
+            setShowAlert(true)
+            setIsLoading(false)
         }
     }
 
@@ -56,53 +70,62 @@ function Signup() {
     }
 
     return (
-        <div className="h-screen w-screen flex flex-col items-center justify-center" style={backgroundStyle}>
-            <h1 className="auth-title text-white">Welcome to Nyan Store</h1>
-            <h3 className="auth-subtitle text-white mt-1">Enter the info below to create account</h3>
-            <form className="form bg-white shadow-md rounded-md p-7 mt-7" onSubmit={handleFormSubmit}>
-                <div className="form-group mt-0">
-                    <label htmlFor="name" className="form-label">
-                        Name:
-                    </label>
-                    <input className="form-input" type="text" name="name" id="name" required ref={nameRef} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email" className="form-label">
-                        Email:
-                    </label>
-                    <input className="form-input" type="email" name="email" id="email" required ref={emailRef} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password" className="form-label">
-                        Password:
-                    </label>
-                    <input
-                        className="form-input"
-                        type="password"
-                        name="password"
-                        id="password"
-                        required
-                        ref={passwordRef}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="passwordConfirm" className="form-label">
-                        Password Confirm:
-                    </label>
-                    <input
-                        className="form-input"
-                        type="password"
-                        name="passwordConfirm"
-                        id="passwordConfirm"
-                        required
-                        ref={passwordConfirmRef}
-                    />
-                </div>
-                <button className="form-btn" type="submit">
-                    Create Account
-                </button>
-            </form>
-        </div>
+        <>
+            <div className="h-screen w-screen flex flex-col items-center justify-center" style={backgroundStyle}>
+                <h1 className="auth-title text-white">Welcome to Nyan Store</h1>
+                <h3 className="auth-subtitle text-white mt-1">Enter the info below to create account</h3>
+                <form className="form bg-white shadow-md rounded-md p-7 mt-7" onSubmit={handleFormSubmit}>
+                    <div className="form-group mt-0">
+                        <label htmlFor="name" className="form-label">
+                            Name:
+                        </label>
+                        <input className="form-input" type="text" name="name" id="name" required ref={nameRef} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email" className="form-label">
+                            Email:
+                        </label>
+                        <input className="form-input" type="email" name="email" id="email" required ref={emailRef} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password" className="form-label">
+                            Password:
+                        </label>
+                        <input
+                            className="form-input"
+                            type="password"
+                            name="password"
+                            id="password"
+                            required
+                            ref={passwordRef}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="passwordConfirm" className="form-label">
+                            Password Confirm:
+                        </label>
+                        <input
+                            className="form-input"
+                            type="password"
+                            name="passwordConfirm"
+                            id="passwordConfirm"
+                            required
+                            ref={passwordConfirmRef}
+                        />
+                    </div>
+                    <button className="form-btn" type="submit">
+                        Create Account
+                    </button>
+                </form>
+            </div>
+            <LoadingWithAlert
+                loading={isLoading}
+                showAlert={showAlert}
+                message={message}
+                error={error}
+                setShowAlert={setShowAlert}
+            />
+        </>
     )
 }
 
